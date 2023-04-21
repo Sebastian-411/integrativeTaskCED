03878b2346295563b4a619586d470c8deee8360c
Curaca <curaca492@gmail.com>
2023-04-21 13:21:14 -0500

    Change Hash node name.

--- src/model/dataStructure/HashNode.java
+++ src/model/dataStructure/HashNode.java
@@ -1,19 +1,20 @@
-public class HNode <K extends Comparable<K>,T> {
+package model.dataStructure;
+public class HashNode<K extends Comparable<K>,T> {
     private K key;
     private T value;
-    private HNode<K,T> next;
-    private HNode<K,T> prev;
+    private HashNode<K,T> next;
+    private HashNode<K,T> prev;
     
-    public HNode<K, T> getPrev() {
+    public HashNode<K, T> getPrev() {
         return prev;
     }
-    public void setPrev(HNode<K, T> prev) {
+    public void setPrev(HashNode<K, T> prev) {
         this.prev = prev;
     }
-    public HNode<K, T> getNext() {
+    public HashNode<K, T> getNext() {
         return next;
     }
-    public void setNext(HNode<K, T> next) {
+    public void setNext(HashNode<K, T> next) {
         this.next = next;
     }
     public K getKey() {
@@ -28,7 +29,7 @@ public class HNode <K extends Comparable<K>,T> {
     public void setValue(T value) {
         this.value = value;
     }
-    public HNode(K key, T value) {
+    public HashNode(K key, T value) {
         this.key = key;
         this.value = value;
     }
