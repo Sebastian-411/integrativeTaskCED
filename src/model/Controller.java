e37069e1c899839a16168921eee1641aed60391d
Curaca <curaca492@gmail.com>
2023-04-22 22:14:58 -0500

    Adding controller atribbutes

--- src/model/Controller.java
+++ src/model/Controller.java
@@ -1,6 +1,49 @@
 package model;
 
+import model.dataStructure.HeapNode;
+import model.dataStructure.Priority;
+import model.dataStructure.standardPriority;
+
+import java.util.ArrayList;
 import java.util.Stack;
 
 public class Controller {
+
+    private int rows;
+    private int columns;
+    private int filesFirstClass;
+    public Controller(int rows, int columns, int filesFirstClass) {
+        this.rows = rows;
+        this.columns = columns;
+        this.filesFirstClass = filesFirstClass;
+    }
+
+    public String outPutOrdering(ArrayList<HeapNode<Priority, Integer>> priorities){
+        priorities.add(new HeapNode<>(new standardPriority(0,0,0,0),11));
+        return "";
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public void setRows(int rows) {
+        this.rows = rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public void setColumns(int columns) {
+        this.columns = columns;
+    }
+
+    public int getFilesFirstClass() {
+        return filesFirstClass;
+    }
+
+    public void setFilesFirstClass(int filesFirstClass) {
+        this.filesFirstClass = filesFirstClass;
+    }
 }
