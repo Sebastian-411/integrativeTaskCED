32b6ab036e5cbb044886e346c5d63979b56e7a6c
Ricardo965 <ricardiox2000@gmail.com>
2023-04-30 22:28:36 -0500

    TestGen corrected

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -2,15 +2,10 @@ package ui;
 
 import model.Controller;
 
+
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-
-        double proportion = (double) 1/4;
-        System.out.println(proportion);
-        System.out.println((1-(double)(1/4))*0.5);
-
-        System.out.println((1 - proportion) * 0.5);
     }
 }
